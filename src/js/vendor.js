0d9eaa964413040b6df60958f63ba2de4c6feca8
@ontheroad_jp <dev@ontheroad.jp>
2020-11-05 16:13:35 +0900

    [New] integrate FontAwesome5 (svg)

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,1 +1,8 @@
+// fontawesome5(svg)
+import {config, dom, library} from '@fortawesome/fontawesome-svg-core';
+import {faDog, faCat} from '@fortawesome/free-solid-svg-icons';
+import {faComments} from '@fortawesome/free-regular-svg-icons';
 
+library.add(faDog,faComments,faCat);
+
+dom.i2svg();
